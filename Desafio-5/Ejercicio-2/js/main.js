3ac4f164f79038e2ed5dfadd78f0a8019b2f4a30
Dev-One <jeiferleon@gmail.com>
2023-06-14 13:54:31 -0500

    Avance

--- Desafio-5/Ejercicio-2/js/main.js
+++ Desafio-5/Ejercicio-2/js/main.js
@@ -1,44 +1,59 @@
+const inputCategoria = document.getElementById("inputNCategoria");
+const contInput = document.getElementById("contenedorInput");
+
 const botoninput = document.getElementById("formContainer");
-const botonAtras = document.getElementById("botonRegresar");
+
+const contInp = document.getElementById("contenedorInput")
 const tabla = document.getElementById("contenedorTabla");
-const formInput = document.getElementById("formContainer");
+const contTh = document.getElementById("contenedorth")
+
+
+inputCategoria.addEventListener("input", gI=>{
+    generarInputs();
+    if(contInp.style.display == "none"){
+        contInp.style.display = "contents"
+        contInp.style.marginTop = "10px";
+    }
+})
 
 botoninput.addEventListener("submit",  b1=> {
     event.preventDefault();
     cambioHTML();
-});
-
-botonAtras.addEventListener("click", b2=>{
-    event.preventDefault();
-    cambioHTML();
+    generarTabla();
 });
 
 function cambioHTML(){
     if (tabla.style.display === "none") {
         tabla.style.display = "table";
-        formInput.style.display = "none";
+    }
+};
 
-    } else {
-        tabla.style.display = "none";
-        formInput.style.display = "flex"
+function generarInputs(){
+    const nInputs = document.getElementById("inputNCategoria").value;
+     contInput.innerHTML = "";
+    
+    for(let i = 0; i < nInputs*2; i++){
+        var divInput = document.createElement("div");
+        divInput.setAttribute("class", "col-5");
+        divInput.style.marginRight = "22px";
+        divInput.style.marginLeft = "22px";
+        divInput.style.marginBottom = "10px";
+
+
+        var nInputCategoria= document.createElement("input");
+        nInputCategoria.setAttribute("class", "form-control");
+        nInputCategoria.setAttribute("id", "inputText" + (i+1));
+        nInputCategoria.setAttribute("type", "text");
+        nInputCategoria.setAttribute("required","");
+        
+        divInput.appendChild(nInputCategoria);
+        contInput.appendChild(divInput);
     }
 };
 
 function generarTabla(){
+    var divTh = document.createElement("th");
+    divTh.textContent = "Ingreso total";
+    contTh.appendChild(divTh)
     
-}
-
-
-function generarInputs(){
-    const nInputs = document.getElementById("inputNCategoria").value;
-    
-    for(let i = 0; i < nInputs ; i++){
-        var newInput = document.createElement("input")
-        newInput.type = "text";
-        newInput.id = "input " + (i + 1);
-        newInput.inputMode = "numeric";
-        newInput.pattern = "[0-9]*";
-
-        contenedor.appendChild(newInput)
-    }
 }
